bbaa29a6575e91ad5ed673102f6c99be76c2b3a6
igitforfun <126478060+igitforfun@users.noreply.github.com>
2025-04-01 20:19:17 +0800

    enhance shell script

--- gitmod.sh
+++ gitmod.sh
@@ -13,10 +13,13 @@ iterate_dir(){
 		#echo $i
 		if [[ $i == *"/.git/" ]]; then  #if it is a git repo
 			pushd $i/../ > /dev/null 2>&1
-			echo -e "\033[1;33m====================================================================================\033[0m" #\033 is meant for font color, [1;33m is color code for yellow
-			echo -e "\033[1;33mOn `pwd`:\033[0m"
-			echo -e "\033[1;33m====================================================================================\033[0m"
-			git status
+			git status | grep '^nothing to commit, working tree clean$' > /dev/null
+			if [[ $? != 0 ]]; then #Parse only those repos that have modified, uncommitted files
+				echo -e "\033[1;33m====================================================================================\033[0m" #\033 is meant for font color, [1;33m is color code for yellow
+				echo -e "\033[1;33mOn `pwd`:\033[0m"
+				echo -e "\033[1;33m====================================================================================\033[0m"
+				git status
+			fi
 			popd > /dev/null 2>&1
 		elif [[ -d $i ]];then # else if it is a directory other than .git/
 			iterate_dir $i
@@ -24,5 +27,8 @@ iterate_dir(){
 	done
 }
 
-currentpath=`pwd`
-iterate_dir $currentpath 
+if [[ -z $1 ]]; then
+	iterate_dir `pwd`
+else
+	iterate_dir $1
+fi
